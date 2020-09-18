d672e67a0b2c6acef1e1c1a23a248a5afeba4156
Thomas Sibley <tsibley@fredhutch.org>
2020-09-18 12:55:18 -0700

    uWSGI (per spec) requires an "application" callable
    Flask is more permissive and also accepts "app".

--- app.py
+++ app.py
@@ -2,4 +2,4 @@ import sys
 from pathlib import Path
 sys.path.insert(0, str(Path(__file__).resolve().parent / "lib"))
 
-from husky_musher.app import app
+from husky_musher.app import app as application
